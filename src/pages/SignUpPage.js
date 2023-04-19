8dd2ccab7b7e728a5825c7b5e8841551d5c4e5d3
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-19 18:09:35 -0300

    Add: signup request, validations and errors handleing

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,28 +1,83 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import MyWalletLogo from "../components/MyWalletLogo";
 
 export default function SignUpPage() {
+  const url = process.env.REACT_APP_API_URL;
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    repeat_password: "",
+  });
+
+  function handleSubmit(e) {
+    console.log(process.env.REACT_APP_API_URL);
+    e.preventDefault();
+    if (form.password !== form.repeat_password) {
+      return alert("Os campos 'Senha' e 'Confirme a senha' devem ser iguais!");
+    }
+    axios
+      .post(`${url}/sign-up`, form)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(`Erro ${err.response.status}: ${err.response.data}`);
+      });
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={handleSubmit}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
+        <input
+          placeholder="Nome"
+          type="text"
+          required
+          value={form.name}
+          onChange={(e) => setForm({ ...form, name: e.target.value })}
+          data-test="name"
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          required
+          value={form.email}
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+          data-test="email"
+        />
         <input
           placeholder="Senha"
           type="password"
-          autocomplete="new-password"
+          autoComplete="new-password"
+          required
+          minLength={3}
+          value={form.password}
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+          data-test="password"
         />
         <input
           placeholder="Confirme a senha"
           type="password"
-          autocomplete="new-password"
+          autoComplete="new-password"
+          required
+          minLength={3}
+          value={form.repeat_password}
+          onChange={(e) =>
+            setForm({ ...form, repeat_password: e.target.value })
+          }
+          data-test="conf-password"
         />
-        <button>Cadastrar</button>
+        <button type="submit" data-test="sign-up-submit">
+          Cadastrar
+        </button>
       </form>
 
-      <Link>Já tem uma conta? Entre agora!</Link>
+      <Link to="/">Já tem uma conta? Entre agora!</Link>
     </SingUpContainer>
   );
 }
