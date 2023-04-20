0fa5011e598993f2d5de018fec3551a2598215cd
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-19 23:01:24 -0300

    Fix: format on deploy

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -76,4 +76,13 @@ const SingInContainer = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  a {
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 18px;
+    color: white;
+    -webkit-text-decoration: none;
+    text-decoration: none;
+    padding-top: 30px;
+  }
 `;
