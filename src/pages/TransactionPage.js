2cb73e5cff47c7595c7ac2f5b7662904279700f0
Viniccius Wirz <vinimovel23@gmail.com>
2023-04-19 22:16:15 -0300

    Add: Transaction page and its logic. Add and subtract function

--- src/pages/TransactionPage.js
+++ src/pages/TransactionPage.js
@@ -1,13 +1,74 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
+import SessionContext from "../contexts/SessionContext";
 
 export default function TransactionsPage() {
+  const params = useParams();
+  const navigate = useNavigate();
+  const url = process.env.REACT_APP_API_URL;
+  const { session, setSession } = useContext(SessionContext);
+  const [form, setForm] = useState({
+    description: "",
+    value: "",
+    type:
+      params.tipo === "entrada"
+        ? "deposit"
+        : params.tipo === "saida" && "withdraw",
+  });
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("session");
+    if (storedToken) {
+      const tokenObj = JSON.parse(storedToken);
+      setSession(tokenObj);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const body = {
+      ...form,
+      value: (Number(parseFloat(form.value).toFixed(2)) * 100).toString(),
+    };
+
+    console.log(body);
+    const config = {
+      headers: {
+        Authorization: `Bearer ${session.token}`,
+      },
+    };
+    axios
+      .post(`${url}/transactions`, body, config)
+      .then(() => {
+        navigate("/home");
+      })
+      .catch((err) => {
+        alert(`Erro ${err.response.status}: ${err.response.data}`);
+      });
+  }
+
   return (
     <TransactionsContainer>
-      <h1>Nova TRANSAÇÃO</h1>
-      <form>
-        <input placeholder="Valor" type="text" />
-        <input placeholder="Descrição" type="text" />
-        <button>Salvar TRANSAÇÃO</button>
+      <h1>Nova {params.tipo}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Valor"
+          type="number"
+          required
+          value={form.value}
+          onChange={(e) => setForm({ ...form, value: e.target.value })}
+        />
+        <input
+          placeholder="Descrição"
+          type="text"
+          required
+          value={form.description}
+          onChange={(e) => setForm({ ...form, description: e.target.value })}
+        />
+        <button type="submit">Salvar {params.tipo}</button>
       </form>
     </TransactionsContainer>
   );
